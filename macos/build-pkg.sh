ac2cf7730a628d61f1785289786ef1dd8a960f51
Markus Mayer <code@mmayer.net>
2023-02-02 11:42:37 -0800

    macos: add various macOS support scripts
    Signed-off-by: Markus Mayer <code@mmayer.net>

---
+++ macos/build-pkg.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+pkg=`basename $0`
+
+ARCHS='x86_64 arm64'
+OUTPUT_DIR='macos/output'
+
+die()
+{
+	echo "$prg: $@" 1>&2
+	exit 1
+}
+
+test -r Makefile || die "script must be run from the top directory"
+
+for arch in $ARCHS; do
+	outdir="$OUTPUT_DIR/$arch"
+	DESTDIR="$outdir/install"
+	export DESTDIR
+	rm -rf "$DESTDIR"
+	test -d "$outdir" || mkdir -p "$outdir"
+	make -j CFLAGS="-O2 -target $arch-apple-macos11" install
+	cgi=`find "$DESTDIR" -name cgit.cgi`
+	strip "$cgi"
+	pkgbuild --root "$DESTDIR" --identifier cgit cgit-$arch.pkg
+	make clean
+	make -C git clean
+done
