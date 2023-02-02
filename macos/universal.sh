ab0cdf1dbf2b6fce0cebea9fbd9b994b5c0f536a
Markus Mayer <code@mmayer.net>
2023-02-02 11:42:37 -0800

    macos: add various macOS support scripts
    Signed-off-by: Markus Mayer <code@mmayer.net>

---
+++ macos/universal.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+prg=`basename $0`
+
+ARCHS='x86_64 arm64'
+OUTPUT_DIR='macos/output'
+
+die()
+{
+	echo "$prg: $@" 1>&2
+	exit 1
+}
+
+for arch in $ARCHS; do
+	outdir="$OUTPUT_DIR/$arch"
+	test -r "$outdir/cgit" || die "$outdir/cgit doesn't exist"
+done
+
+lipo -create -output "$OUTPUT_DIR/cgit" "$OUTPUT_DIR/arm64/cgit" \
+	"$OUTPUT_DIR/x86_64/cgit"
