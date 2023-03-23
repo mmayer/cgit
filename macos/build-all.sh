f55575c9df30b47b85c99d6ca1394beb73cfd05b
Markus Mayer <code@mmayer.net>
2023-03-22 17:10:49 -0700

    macos/build-pkg.sh: add command line option "--skip"
    Command line option "--skip" builds everything, but skips the generation
    of macOS packages (.pkg) files.
    
    Consequently, the build-all.sh script can now simply call
        build-pkg.sh --skip.
    
    Signed-off-by: Markus Mayer <code@mmayer.net>

--- macos/build-all.sh
+++ macos/build-all.sh
@@ -1,23 +1,3 @@
 #!/bin/sh
-prg=`basename $0`
 
-ARCHS=${ARCHS:-'arm64 x86_64'}
-OUTPUT_DIR='macos/output'
-
-die()
-{
-	echo "$prg: $@" 1>&2
-	exit 1
-}
-
-test -r Makefile || die "script must be run from the top directory"
-
-for arch in $ARCHS; do
-	outdir="$OUTPUT_DIR/$arch"
-	test -d "$outdir" || mkdir -p "$outdir"
-	make -j CFLAGS="-O2 -target $arch-apple-macos11"
-	strip cgit
-	mv cgit "$outdir"
-	make clean
-	make -C git clean
-done
+exec `dirname $0`/build-pkg.sh --skip $@
--- macos/build-pkg.sh
+++ macos/build-pkg.sh
@@ -3,12 +3,15 @@ pkg=`basename $0`
 
 ARCHS=${ARCHS:-'arm64 x86_64'}
 OUTPUT_DIR='macos/output'
+skip_mode=0
 
 die()
 {
 	echo "$prg: $@" 1>&2
 	exit 1
 }
+
+[ "$1" = "--skip" ] && skip_mode=1
 
 test -r Makefile || die "script must be run from the top directory"
 
@@ -18,11 +21,21 @@ for arch in $ARCHS; do
 	export DESTDIR
 	rm -rf "$DESTDIR"
 	test -d "$outdir" || mkdir -p "$outdir"
-	make -j CFLAGS="-O2 -target $arch-apple-macos11.3" install || exit 1
+	PATH=/usr/bin:/bin make -j \
+		LUA_PKGCONFIG=lua \
+		CFLAGS="-O2 -target $arch-apple-macos11.3" \
+		LUA_CFLAGS="-I`pwd`/../lua" \
+		LUA_LIBS="`pwd`/../lua/liblua-$arch.a" \
+		install || exit 1
 	cgi=`find "$DESTDIR" -name cgit.cgi`
 	strip "$cgi"
-	pkgbuild --root "$DESTDIR" --scripts `dirname $0`/pkg-scripts \
-		--identifier cgit cgit-$arch.pkg || exit 1
+	if [ $skip_mode = 0 ]; then
+		pkgbuild --root "$DESTDIR" --scripts `dirname $0`/pkg-scripts \
+			--identifier cgit cgit-$arch.pkg || exit 1
+	fi
 	make clean
 	make -C git clean
+	echo "=================================================="
+	ls -l "$cgi"
+	echo "=================================================="
 done
